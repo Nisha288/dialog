d9bdf4676dc8cffff705b8ca47d4a9dfb1053445
Nisha Anand <nisha.anand@invenio-solutions.com>
2019-11-04 13:02:05 +0000

    first

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,8 +4,34 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("valuedialog.valuedialog.controller.View1", {
-		onInit: function () {
+		onValueHelp: function (oEvent) {
 
-		}
+			var oDialog = new sap.ui.xmlfragment("valuedialog.valuedialog.fragment.selectdialog", this);
+
+			this.getView().addDependent(oDialog);
+
+			oDialog.bindAggregation("items", {
+
+				path: "city>/city",
+
+				template: new sap.m.StandardListItem({
+
+					title: "{city>cityname}"
+
+				})
+
+			});
+			oDialog.open();
+		},
+		_handleValueHelpClose: function (evt) {
+
+				var oSelectedItem = evt.getParameter("selectedItem");
+				if (oSelectedItem) {
+					var productInput = this.byId("input1");
+					productInput.setValue(oSelectedItem.getTitle());
+				}
+				evt.getSource().getBinding("items").filter([]);
+			}
+
 	});
 });
